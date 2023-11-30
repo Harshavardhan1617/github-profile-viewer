5dac407f0ea96ffa1b77fdccd2368bc4c17321a3
Harshavardhan1617 <harshavardhan1617@gmail.com>
2023-11-30 21:34:07 +0530

    created dom and integrated api

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
-const form = document.querySelector('form')
-const button = document.querySelector('#submit')
-const userName = document.querySelector('#username')
-const userInfo = document.querySelector('#userinfo')
-const displayName = document.createElement('LI')
-const avatar = document.querySelector('#avatar')
-const bio = document.createElement('LI')
+const domObjects = ["form", "submit", "username", "userInfo", "avatar", "bio", "repoLink", "location", "twitterHandle"]
 
+const arrMap = domObjects.map((dom) => {
+    return document.querySelector(`#${dom}`)
+})
 
-form.addEventListener('submit', (e) => {
+arrMap[0].addEventListener('submit', (e) => {
     e.preventDefault()
+    const userName = arrMap[2].value
+    gitRequestUser(userName)
 })
 
-button.addEventListener('click', () => {
-    console.log(userName.value)
-    gitResponse(userName.value)
-})
 
-const gitResponse = async(user) => {
-    const data = await fetch(`https://api.github.com/users/${user}`)
-    const loadedData = await data.json()
-    console.log(loadedData.name)
-    displayName.innerText = loadedData.name
-    userInfo.append(displayName)
-    avatar.src = loadedData.avatar_url
-    userInfo.append(avatar)
-    bio.innerText = loadedData.bio
-    userInfo.append(bio)
 
+const gitRequestUser = async(usr) => {
+    try {
+        const res = await fetch(`https://api.github.com/users/${usr}`)
+        const data = await res.json()
+        // console.log(data)
+        const dp = createGeneric("img", data.avatar_url)
+        arrMap[4].append(dp)
+    } catch (error) {
+        console.log(error)
+    }
+ 
+
+
+} 
+
+const testFunction = (str) => {
+    console.log(str)
 }
+
+let element = ""
+const createGeneric = (ele, text, link) => {
+    if (ele.toUpperCase() === "IMG"){
+        element = document.createElement('IMG')
+        element.src = text
+        return element
+    }
+    if (ele.toUpperCase() === "A"){
+        element = document.createElement('A')
+        element.innerText = text
+        element.href = link
+        // console.log(element)
+    }
+    else{
+        element = document.createElement(`${ele}`)
+        element.innerText = text
+        // console.log(element)
+    }
+    
+    
+}
+
 
 
