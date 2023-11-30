5ed8576f524d804feb1438dc2af9c2c2589c0315
Harshavardhan1617 <harshavardhan1617@gmail.com>
2023-11-30 17:34:52 +0530

    working on usrinfo UI

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const form = document.querySelector('form')
 const button = document.querySelector('#submit')
 const userName = document.querySelector('#username')
+const userInfo = document.querySelector('#userinfo')
+const displayName = document.createElement('LI')
+const avatar = document.querySelector('#avatar')
+const bio = document.createElement('LI')
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -13,7 +18,15 @@ button.addEventListener('click', () => {
 
 const gitResponse = async(user) => {
     const data = await fetch(`https://api.github.com/users/${user}`)
-    console.log("user name is ", data.name )
+    const loadedData = await data.json()
+    console.log(loadedData.name)
+    displayName.innerText = loadedData.name
+    userInfo.append(displayName)
+    avatar.src = loadedData.avatar_url
+    userInfo.append(avatar)
+    bio.innerText = loadedData.bio
+    userInfo.append(bio)
+
 }
 
 
